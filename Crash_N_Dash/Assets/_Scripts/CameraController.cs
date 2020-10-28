2624eea3238f118d316d9b0d9cf45c2446582251
Charlie Conneely <g00348887@gmit.ie>
2020-10-28 01:34:58 +0000

    make-shift player moving on road

---
+++ Crash_N_Dash/Assets/_Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private Transform player;
+    private float yOffset = 2f;
+    private float zOffset = -10f;
+    
+    void Start()
+    {
+        player = GameObject.Find("Player").transform;
+    }
+
+    void LateUpdate()
+    {
+        transform.position = new Vector3(player.position.x, player.position.y + yOffset, player.position.z + zOffset);        
+    }
+}
---
+++ Crash_N_Dash/Assets/_Scripts/PlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float speed = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float hMovement = Input.GetAxis("Horizontal") * speed;
+        float vMovement = Input.GetAxis("Vertical") * speed / 2;
+
+        transform.Translate(new Vector3(hMovement, 0, vMovement) * Time.deltaTime);
+    }
+}
