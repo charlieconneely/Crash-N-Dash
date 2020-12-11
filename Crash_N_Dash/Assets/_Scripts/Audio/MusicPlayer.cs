7b56be7cacaa30f58c60283fc4ee84c741f0d464
Charlie Conneely <g00348887@gmit.ie>
2020-12-11 16:07:06 +0000

    splash screen + bg music added

---
+++ Crash_N_Dash/Assets/_Scripts/Audio/MusicPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour
+{
+    private void Awake() {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Start() {
+        Invoke("LoadMainMenu", 3);
+    }
+
+    private void LoadMainMenu() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
